5b486ff00182d8b04e8757ee549c85d606a33651
Aditi Sachdeva <aditisachdeva9159@gmail.com>
2025-09-03 17:55:53 +0530

    Arrays : Add Build Array from Permutation

---
+++ arrays/BuildArrayFromPermutation.java
@@ -0,0 +1,26 @@
+package arrays;
+
+/**
+ * Problem: Build Array from Permutation
+ * Link: https://leetcode.com/problems/build-array-from-permutation/
+ * Time Complexity: O(n)
+ * Space Complexity: O(n)
+ */
+
+
+import java.util.Arrays;
+
+public class BuildArrayFromPermutation {
+    public static void main(String[] args) {
+        int[] arr = {5,0,1,2,3,4};
+        System.out.println(Arrays.toString(buildArray(arr)));
+    }
+
+    public static int[] buildArray(int[] arr){
+        int[] ans = new int[arr.length];
+        for(int i = 0;i<arr.length;i++){
+            ans[i] = arr[arr[i]];
+        }
+        return ans;
+    }
+}
