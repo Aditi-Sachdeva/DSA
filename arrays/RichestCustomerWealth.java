53f4ef6d6d664a3519122aad4aa218dfae10e6df
Aditi Sachdeva <aditisachdeva9159@gmail.com>
2025-09-03 18:54:24 +0530

    Arrays: Add Richest Customer Wealth

---
+++ arrays/RichestCustomerWealth.java
@@ -0,0 +1,37 @@
+package arrays;
+
+/**
+ * Problem: Richest Customer Wealth
+ * Link: https://leetcode.com/problems/richest-customer-wealth/
+ * Time Complexity: O(m × n)
+ * Space Complexity: O(1) → only uses scalar variables
+ */
+
+public class RichestCustomerWealth {
+    public static void main(String[] args) {
+        int[][] accounts = {
+            {1,5},
+            {7,3},
+            {3,5}
+        };
+
+        System.out.println(maximumWealth(accounts));
+    }
+
+    public  static int maximumWealth(int[][] accounts) {
+        int max = 0;
+        for(int[] customer : accounts){
+            int sum = sumOfArray(customer);
+            max = Math.max(max,sum);
+        }
+        return max;
+    }
+
+    public static int sumOfArray(int[] arr){
+        int sum = 0;
+        for(int money  : arr){
+            sum += money;
+        }
+        return sum;
+    }
+}
