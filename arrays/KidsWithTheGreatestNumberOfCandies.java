c407df4e15223bfa80f7ecb360e0f9a97380642f
Aditi Sachdeva <aditisachdeva9159@gmail.com>
2025-09-04 13:36:22 +0530

    Arrays: Add Kids With the Greatest Number of Candies

---
+++ arrays/KidsWithTheGreatestNumberOfCandies.java
@@ -0,0 +1,31 @@
+package arrays;
+
+/**
+ * Problem: Kids With the Greatest Number of Candies
+ * Link: https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/
+ * Time Complexity: O(n)
+ * Space Complexity: O(n)
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class KidsWithTheGreatestNumberOfCandies{
+    public static void main(String[] args) {
+        int candies[] = {4,2,1,1,2};
+        int extraCandies = 1;
+        System.out.println(kidsWithCandies(candies, extraCandies));
+        
+    }
+        public static  List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean> ans = new ArrayList<>();
+        int max = 0;
+        for(int c : candies){
+            max = Math.max(c,max);
+        }
+        for(int c : candies){
+            ans.add(c + extraCandies >= max);
+        }
+        return ans;
+    }
+}
