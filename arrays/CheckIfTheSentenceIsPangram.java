70e84b13ab07147a5d6e30b04bc9f6751fa81f31
Aditi Sachdeva <aditisachdeva9159@gmail.com>
2025-09-04 18:04:37 +0530

    Arrays: Add Check if the Sentence Is Pangarm

---
+++ arrays/CheckIfTheSentenceIsPangram.java
@@ -0,0 +1,29 @@
+package arrays;
+
+/**
+ * Problem: Check if the Sentence Is Pangram
+ * Link: https://leetcode.com/problems/check-if-the-sentence-is-pangram/
+ * Time Complexity: O(n) 
+ * Space Complexity: O(1) 
+ */
+
+
+public class CheckIfTheSentenceIsPangram {
+    public static void main(String[] args) {
+        String sentence = "thequickbrownfoxjumpsoverthelazydog";
+        System.out.println(checkIfPangram(sentence));
+    }
+    public static boolean checkIfPangram(String sentence) {
+        boolean[] arr = new boolean[26];
+        for(char ch : sentence.toCharArray()){
+            arr[ch-'a'] = true;
+        }
+        for(boolean ch : arr){
+            if(!ch){
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
