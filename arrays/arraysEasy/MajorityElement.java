2182b696d3db04804caafe3790fc9ad5e3c90e3a
Aditi Sachdeva <aditisachdeva9159@gmail.com>
2025-09-07 16:53:55 +0530

    Arrays: Add Majority Element

---
+++ arrays/arraysEasy/MajorityElement.java
@@ -0,0 +1,34 @@
+package arrays.arraysEasy;
+
+/**
+ * Problem: Majority Element
+ * Link: https://leetcode.com/problems/majority-element/
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ */
+
+
+public class MajorityElement {
+    public static void main(String[] args) {
+        int[] nums = {2,2,1,1,1,2,2};
+        System.out.println(majorityElement(nums));
+    }
+    public static int majorityElement(int[] nums) {
+        int n = nums.length;
+        int count =0;
+        int temp=0;
+        for(int i=0;i<n;i++){
+            if(count == 0){
+                temp = nums[i];
+                count++;
+            }
+            else if(nums[i] == temp){
+                count++;
+            }
+            else{
+                count--;
+            }
+        }
+        return temp;
+    }
+}
