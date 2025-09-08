5c766073ac583070ee09e47f544a7b5b16278950
Aditi Sachdeva <aditisachdeva9159@gmail.com>
2025-09-08 20:21:13 +0530

    Arrays: Pending Intersection Of Two Arrays

---
+++ arrays/pending/IntersectionOfTwoArrays.java
@@ -0,0 +1,12 @@
+package arrays.pending;
+
+/**
+ * Problem: Intersection of Two Arrays
+ * Link: https://leetcode.com/problems/intersection-of-two-arrays/
+ * Status: ✅ Solved using sorting + two pointers
+ * Note: Will revisit with HashSet-based approach once hashing is covered
+ */
+
+public class IntersectionOfTwoArrays {
+    
+}
