afb74fb35e6d2be4a216cbcee938f849613fff60
Aditi Sachdeva <aditisachdeva9159@gmail.com>
2025-09-12 20:53:51 +0530

    Arrays: Add Find Peak Element

---
+++ arrays/arraysMedium/FindPeakElement.java
@@ -0,0 +1,41 @@
+package arrays.arraysMedium;
+
+/**
+- Problem: Find Peak Element
+- Link: https://leetcode.com/problems/find-peak-element/
+- Time Complexity: O(log n)
+- Space Complexity: O(1)
+*/
+
+public class FindPeakElement {
+    public static void main(String[] args) {
+        int[] nums = {1,2,1,3,5,6,4};
+        System.out.println(findPeakElement(nums));
+    }
+    public static int findPeakElement(int[] nums) {
+        int n = nums.length;
+        int low = 1;
+        int high = n - 2;
+        if (n == 1) {
+            return 0;
+        }
+        if (nums[0] > nums[1]) {
+            return 0;
+        }
+        if (nums[n - 1] > nums[n - 2]) {
+            return n - 1;
+        }
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) {
+                return mid;
+            } else if (nums[mid] > nums[mid - 1]) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
