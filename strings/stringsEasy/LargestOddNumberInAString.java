264abb1840f3a29baa1cb4d03e6c2be6b24aa9e2
Aditi Sachdeva <aditisachdeva9159@gmail.com>
2025-09-17 20:50:31 +0530

    Strings: Add Largest Odd Number in a String

---
+++ strings/stringsEasy/LargestOddNumberInAString.java
@@ -0,0 +1,25 @@
+package strings.stringsEasy;
+
+/**
+ * Problem: Largest Odd Number in a String
+ * Link: https://leetcode.com/problems/largest-odd-number-in-string/
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ */
+
+public class LargestOddNumberInAString {
+    public static void main(String[] args) {
+        String num = "35278";
+        System.out.println(largestOddNumber(num));
+    }
+    public static String largestOddNumber(String num) {
+        for(int i = num.length()-1;i>=0;i--){
+            char ch = num.charAt(i);
+            if((ch-'0')%2 == 1){
+                return num.substring(0,i+1);
+            }
+        }
+        return "";
+    }
+}
+
