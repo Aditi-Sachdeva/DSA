7d02e116878af80ce531247e0c46b1b8e2fb728d
Aditi Sachdeva <aditisachdeva9159@gmail.com>
2025-09-23 09:59:37 +0530

    Strings: Add Long Pressed Name

---
+++ strings/stringsEasy/LongPressedName.java
@@ -0,0 +1,33 @@
+package strings.stringsEasy;
+
+/**
+ * Problem: Long Pressed Name
+ * Link: https://leetcode.com/problems/long-pressed-name/
+ * Time Complexity: O(n + m)
+ * Space Complexity: O(1)
+ */
+
+public class LongPressedName {
+    public static void main(String[] args) {
+        String name = "alex";
+        String typed = "aaleex";
+        System.out.println(isLongPressedName(name, typed));
+    }
+    public static boolean isLongPressedName(String name, String typed) {
+        int i=0;
+        int j=0;
+        while(j<typed.length()){
+            if(i<name.length() && name.charAt(i)==typed.charAt(j)){
+                i++;
+                j++;
+            }
+            else if(j>0 && typed.charAt(j) == typed.charAt(j-1)){
+                j++;
+            }
+            else{
+                return false;
+            }
+        }
+        return i==name.length();
+    }
+}
