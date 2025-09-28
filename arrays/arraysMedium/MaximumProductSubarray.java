820a84797a1d5159381b3a9e0d33f6667b70e9e3
Aditi Sachdeva <aditisachdeva9159@gmail.com>
2025-09-28 13:15:34 +0530

    Arrays: Add Maximum Product Subarray

---
+++ arrays/arraysMedium/MaximumProductSubarray.java
@@ -0,0 +1,33 @@
+package arrays.arraysMedium;
+
+/**
+- Problem: Maximum Product Subarray
+- Link: https://leetcode.com/problems/maximum-product-subarray/
+- Time Complexity: O(n)
+- Space Complexity: O(1)
+*/
+
+public class MaximumProductSubarray {
+    public static void main(String[] args) {
+        int[] nums = {-1,-5,-7,7,9,4};
+        System.out.println(maxProduct(nums));
+    }
+    public static int maxProduct(int[] nums) {
+        int prefix = 1;
+        int suffix = 1;
+        int max = Integer.MIN_VALUE;
+        int n = nums.length;
+        for(int i=0;i<n;i++){
+            if(prefix == 0){
+                prefix = 1;
+            }
+            if(suffix == 0){
+                suffix = 1;
+            }
+            prefix = prefix * nums[i];
+            suffix = suffix * nums[n-i-1];
+            max = Math.max(max,Math.max(prefix,suffix));
+        }
+        return max;
+    }
+}
