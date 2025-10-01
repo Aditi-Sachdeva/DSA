d679ba88f37cbd30534bb8bfb1a1522005d750ce
Aditi Sachdeva <aditisachdeva9159@gmail.com>
2025-10-01 14:27:44 +0530

    Arrays: Add Search a 2D Matrix II

---
+++ arrays/arrays2D/SearchA2DMatrixII.java
@@ -0,0 +1,34 @@
+package arrays.arrays2D;
+
+/**
+ * Problem: Search a 2D Matrix II
+ * Link: https://leetcode.com/problems/search-a-2d-matrix-ii/
+ * Time Complexity: O(n + m)
+ * Space Complexity: O(1)
+ */
+
+public class SearchA2DMatrixII {
+    public static void main(String[] args) {
+        int[][] matrix = {{1,4,7,11,15},{2,5,8,12,19},{3,6,9,16,22},{10,13,14,17,24},{18,21,23,26,30}};
+        int target = 5;
+        System.out.println(searchMatrix(matrix, target));
+    }
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int row = 0;
+        int col = m-1;
+        while(row < n && col >= 0){
+            if(matrix[row][col] == target){
+                return true;
+            }
+            else if(matrix[row][col] > target){
+                col--;
+            }
+            else{
+                row++;
+            }
+        } 
+        return false;
+    }
+}
